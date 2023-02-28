f9303150fa45ad8d3f206aa3c0f6214215021875
Dalton <lambertd757@gmail.com>
2023-02-28 13:48:15 -0500

    Created UserDao interface, added getAllUsers and createUser methods

---
+++ src/main/java/com/dalton/WeatherApp/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.dalton.WeatherApp.dao;
+
+import com.dalton.WeatherApp.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    List<User> getAllUsers();
+
+    User createUser(User user);
+}
